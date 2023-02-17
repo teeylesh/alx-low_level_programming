5226f12f6d9d6c55a2468f0e50fd510642c490d0
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-17 10:25:22 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,20 +2,21 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: prints alphabet in lowercase then in uppercase Return: Always 0 (Success)
+ * Description: prints alphabet in lowercase then in uppercase
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int ch;
+	int ch;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-        putchar(ch);
-}
-for (ch = 'A'; ch <= 'Z'; ch++)
-{
-        putchar(ch);
-}
-putchar('\n');
-return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+        	putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+        	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }

9fec92109eef90463ce4b2e38048486d6405f6de
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-17 10:56:16 -0800

    corrections to spacing errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-        int ch;
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-        for (ch = 'A'; ch <= 'Z'; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }

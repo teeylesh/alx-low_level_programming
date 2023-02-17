70ebb69542de93afe04f492b894c4aa58e30ebec
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-17 09:18:28 -0800

    Task 0 Corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -13,7 +12,6 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 
 if (n > 0)
 {	

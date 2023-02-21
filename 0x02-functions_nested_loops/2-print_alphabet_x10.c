01c6376c50e5ce50b8765d3c468778d39f503145
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-21 13:50:12 -0800

    Tasl 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  * Description: prints 10 times the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char y;
 	int g = 0;

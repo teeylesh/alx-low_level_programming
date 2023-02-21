a8852c0ebff422006d5c1fff7ae2636df9e32113
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-21 14:24:31 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/** _islower - Entry point
+ * Description: checks for lowercase character C
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	int g = 'a';
+
+	for (g = 'a'; g <= 'z'; g++);
+	{
+		if (c == 'c')
+		{
+			return(1);
+		}
+		else
+		{
+			return(0);
+		}
+	}
+}

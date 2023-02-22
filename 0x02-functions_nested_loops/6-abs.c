be3fe64720957b1f8678b45d1d44e511a405612e
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 05:40:49 -0800

    Task 6 ammended

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,14 +4,21 @@
 /**
  * _abs - Entry point
  * Description: computes the absolute value of an integer
- * @n: parameter to check
- * Return: value of integer
+ * @n: int to check
+ * Return: absolute value of integer
  */
 int _abs(int)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
 		return (n);
 	}
-	return (-n);
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }

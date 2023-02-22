a39f2151235950cbfbe1dfc27b712f354d1a0953
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 12:03:20 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @num1: parameter for first integer
+ * @num2: parameter for second integer
+ * Description: adds two integers and returns result
+ * Return: sum of both integers
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}

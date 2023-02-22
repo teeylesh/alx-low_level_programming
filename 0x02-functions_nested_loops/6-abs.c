1cecbb5540a8772dafd881880c49e91554dda923
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 05:43:08 -0800

    Task 6 corrected

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,11 @@
 #include "main.h"
 /**
  * _abs - Entry point
+ * @n: n is an integer
  * Description: computes the absolute value of an integer
- * @n: int to check
  * Return: absolute value of integer
  */
-int _abs(int)
+int _abs(int n)
 {
 	if (n > 0)
 	{

0dc07beae110f6c7b4dfc47d707c03f403b6e859
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 06:01:49 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.>
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: n is an integer
+ * Description: prints last digit of a number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int dig = n % 10;
+
+	if (n < 0)
+	{
+		dig = dig * -1;
+	}
+	_putchar(dig + '0');
+	return (dig);
+}

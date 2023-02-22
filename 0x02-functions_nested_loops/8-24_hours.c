bcd863dcb0e530f79b6e61e84b65ca99249f129b
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 06:29:16 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Description: prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int d, e;
+
+	d = 0;
+
+	while (d < 24)
+	{
+		e = 0;
+		while (e < 59)
+		{
+			_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
+			_putchar(':');
+			_putchar((e / 10) + '0');
+			_putchar((e % 10) + '0');
+			_putchar('\n');
+			e++;
+		}
+d++;
+	}
+}

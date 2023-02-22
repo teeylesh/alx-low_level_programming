60ed057634333e7fde8d643b1b565cc66611e1ea
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-22 14:32:54 -0800

    Task 12 corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,44 +9,42 @@
 void print_times_table(int n)
 {
 	int t, d, e;
-	 
-	 if (n >= 0 && n <= 15)
-	 {
-		 for (t = 0; t <= n; t++)
-		 {
-			 for (d = 0; d <= n; d++)
-			 {
-				 e = d * t;
-				 if (d == 0)
-				 {
-					 _putchar(e + '0');
-				 }
-				 else if (e < 10 && d != 0)
-				 {
+
+	if (n >= 0 && n <= 15)
+	{
+		for (t = 0; t <= n; t++)
+		{
+			for (d = 0; d <= n; d++)
+			{
+				e = d * t;
+				if (d == 0)
+				{
+					_putchar(e + '0');
+				} else if (e < 10 && d != 0)
+				{
 					 _putchar(',');
 					 _putchar(' ');
 					 _putchar(' ');
 					 _putchar(' ');
 					 _putchar(e + '0');
-				 }
-				 else if (e >= 10 && e < 100)
-				 {
-					 _putchar(',');
-					 _putchar(' ');
-					 _putchar(' ');
-					 _putchar((e / 10) + '0');
-					 _putchar((e % 10) + '0');
-				 }
-				 else if (e >= 100)
-				 {
+				} else if (e >= 10 && e < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((e / 10) + '0');
+					_putchar((e % 10) + '0');
+				}
+				else if (e >= 100)
+				{
 					 _putchar(',');
 					 _putchar(' ');
 					 _putchar((e / 100) + '0');
-					 _putchar(((e /10) % 10) + '0');
+					 _putchar(((e / 10) % 10) + '0');
 					 _putchar((e % 10) + '0');
-				 }
-			 }
-			 _putchar('\n');
-		 }
-	 }
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }

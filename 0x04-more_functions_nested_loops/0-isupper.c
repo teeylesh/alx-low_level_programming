09aba41cac710128513e77521fb913e674ef95d8
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-24 13:03:31 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if true, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	int c;
+	int j;
+
+	for (c = 'A'; c <= 'Z'; a++)
+	{
+		if (j = c)
+			return (1);
+		else
+			return (0);
+	}
+}

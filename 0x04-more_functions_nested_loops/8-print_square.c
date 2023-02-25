36d5e09b28a478d582bd2801979d2989008288d3
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-24 16:08:50 -0800

    Task 8! My precious, precious sqare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square followed by a new line
+ * @size: size of square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

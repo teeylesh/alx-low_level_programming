b066c745b9db15eb3987735e0d98e05637266101
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-27 09:46:03 -0800

    TRask 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - opies the string pointed to by src, including the terminating null
+ * byte (\0), to the buffer pointed to by dest
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	_strcpy (dest, src);
+	printf("_strcpy( dest, src) :  %s\n", dest );
+
+	return (dest);
+}

66143318f8ce0cf9532d3e51ef8bc9859b72f10b
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-27 06:20:20 -0800

    Task 0

---
+++ 0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an integer and updates its value to 98
+ * @n : parameter to be checked
+ * return: value of parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

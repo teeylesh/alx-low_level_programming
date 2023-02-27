5619dd3b7fa303678dc12dbfb43d9388abe8a763
Tobiloba Eleshin <teeylesh@gmail.com>
2023-02-27 09:51:34 -0800

    Task 9 updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,12 +3,20 @@
 /**
  * _strcpy - opies the string pointed to by src, including the terminating null
  * byte (\0), to the buffer pointed to by dest
+ * @src: parameter to be checked
+ * @dest: parameter to be checked
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	_strcpy (dest, src);
-	printf("_strcpy( dest, src) :  %s\n", dest );
+	int g = -1;
+
+	do
+	{
+		g++;
+		dest[g] = src[g];
+	}
+	while (src[g] != '\0');
 
 	return (dest);
 }
